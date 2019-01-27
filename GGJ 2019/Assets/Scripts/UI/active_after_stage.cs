e4dddf3443dfafa71cf93e1a10078d95fc5f56f7
Dmcdominic <dmcdominic@gmail.com>
2019-01-26 21:06:13 -0500

    Key controls prompt added

---
+++ GGJ 2019/Assets/Scripts/UI/active_after_stage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class active_after_stage : MonoBehaviour {
+
+	public move_stage activation_stage;
+	public GameObject to_toggle_active;
+
+	// Update is called once per frame
+	void Update() {
+		if (movement.move_Stage >= activation_stage) {
+			to_toggle_active.SetActive(true);
+		} else {
+			to_toggle_active.SetActive(false);
+		}
+	}
+}
---
+++ GGJ 2019/Assets/Scripts/UI/prompt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prompt : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "bird") {
+			GetComponentInChildren<Animator>().SetBool("revealed", true);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collider) {
+		if (collider.gameObject.tag == "bird") {
+			GetComponentInChildren<Animator>().SetBool("revealed", false);
+		}
+	}
+}
